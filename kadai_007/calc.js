eec598f0877a266e773eede582bf1346b2c40c9d
kdokusi <139143186+kdokusi@users.noreply.github.com>
2023-08-13 01:37:08 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+
+let num = Math.floor(Math.random() * 16)
+console.log(num);
+
+if ( num=== 15) {
+  console.log('3の倍数です')
+}
+
+if ( num===10) {
+  console.log('5の倍数です')
+}
+else if( num===15){
+console.log('3と5の倍数です');
+}
+
+else {
+  console.log ('それ以外');
+}
