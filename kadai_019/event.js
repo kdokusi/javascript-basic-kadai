71b9330b235669eaf434a01b7b8e25a4266f0b18
kdokusi <139143186+kdokusi@users.noreply.github.com>
2023-08-24 02:08:56 +0900

    kadai_019

---
+++ kadai_019/event.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const text=document.getElementById('text');
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click',()=> {
+
+  text.textContent ='ボタンをクリックしました';
+});
